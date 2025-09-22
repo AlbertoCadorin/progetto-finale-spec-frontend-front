d9e4d0ea95d91112050d12ccef3974756e4f020d
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-09-22 15:32:53 +0200

    feat: add Bootstrap icons for Wishlist and ProductCard buttons; update Navbar and ProductCard components

--- components/ProductsCard.jsx
+++ components/ProductsCard.jsx
@@ -44,7 +44,13 @@ const ProductCard = ({ productId }) => {
                 </Link>
                 <div className="card-body ">
                     <button onClick={handleWishlistClick} className={`btn ${wishlist.find(p => p.id === productId) ? 'btn-danger' : 'btn-primary'}`}>
-                        {wishlist.find(p => p.id === productId) ? 'Rimuovi dalla wishlist' : 'Aggiungi alla wishlist'}
+                        {wishlist.find(p => p.id === productId)
+                            ? <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
+                            </svg> :
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
+                            </svg>}
                     </button>
                 </div>
             </div>
