23ba86cb298ac4c64c62ce05ea9c5663d5e58bec
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-09-26 10:08:36 +0200

    feat: add Product type definition and product data in JSON format

---
+++ types.ts
@@ -0,0 +1,16 @@
+export type Product = {
+    title: string;
+    category: string;
+    releaseYear: number;
+    price: number;
+    memory: number;
+    colors: string;
+    sim: string;
+    system: string;
+    display: string;
+    ram: number;
+    battery: number;
+    camera: string;
+    camera1: string;
+    image: string
+};
